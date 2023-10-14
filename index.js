9b50ba80f4313114c4bb165bae1ea1453a243bba
Saania Altaf <142258260+saaniaaltaf121@users.noreply.github.com>
2023-10-15 03:00:28 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,32 @@
+// chapter 1
+// exercise:1.1
+// Question 1
+console.log(10+4)
+// Question 2
+console.log("Hello world!");
+
+// exercise 1.2
+
+alert("Saying hi from a different file!");
+
+
+// Practice exercise 1.3
+// Create a separate file called app with the extension .js.
+// 2. Within the .js file, add some JavaScript code.
+// 3. Link to the separate .js file within the HTML file you created in Practice
+// exercise 1.2.
+// 4. Open the HTML file within your browser and check to see whether the
+// JavaScript code ran properly.
+
+
+// Practice exercise 1.4
+// 1. Add a new statement to your JavaScript code by setting a variable value.
+// Since we will cover this in the next chapter, you can use the following line:
+// let a = 10;
+// 2. Add a comment at the end of the statement indicating that you set a value of
+// 10.
+// 3. Print the value using console.log(). Add a comment explaining what this
+// will do.
+
+let a = 10;
+console.log(a);
